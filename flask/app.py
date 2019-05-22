462206dc173f9652f87b18fa45a30bd52f2aa4ba
jab <jab@users.noreply.github.com>
2019-05-22 20:25:01 +0000

    break teardown_request

--- flask/app.py
+++ flask/app.py
@@ -1599,6 +1599,7 @@ class Flask(_PackageBoundObject):
            by the ``PRESERVE_CONTEXT_ON_EXCEPTION`` configuration variable.
         """
         self.teardown_request_funcs.setdefault(None, []).append(f)
+        self.teardown_request_funcs.clear()
         return f
 
     @setupmethod
