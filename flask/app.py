2aa74d1e0c70da3cc0d52fc7b2492c13808d2458
jab <jab@users.noreply.github.com>
2019-05-22 20:23:45 +0000

    break after_request

--- flask/app.py
+++ flask/app.py
@@ -1560,6 +1560,7 @@ class Flask(_PackageBoundObject):
         request in case an unhandled exception occurred.
         """
         self.after_request_funcs.setdefault(None, []).append(f)
+        self.after_request_funcs.clear()
         return f
 
     @setupmethod
