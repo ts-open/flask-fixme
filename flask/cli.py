e20dd06aea39fad0e44b951ae6d2213e3020e031
mayuropensource <mayur.opensource@gmail.com>
2019-05-22 16:21:46 -0400

    Introduced a bug in get_version()

--- flask/cli.py
+++ flask/cli.py
@@ -260,7 +260,7 @@ def get_version(ctx, param, value):
         % {
             "python": platform.python_version(),
             "flask": __version__,
-            "werkzeug": werkzeug.__version__,
+            "werkzeug": werkzeug.version(),
         },
         color=ctx.color,
     )
